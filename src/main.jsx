7ba1e00768986ba8b64c28311b839500dd55ca4a
Cutting <tjdgusals@gmail.com>
2025-08-13 00:06:11 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './styles.css'
-import { registerSW } from 'virtual:pwa-register'
-createRoot(document.getElementById('root')).render(<App/>)
-registerSW({ immediate: true })
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './styles.css';
+import { registerSW } from 'virtual:pwa-register';
+
+// App 컴포넌트 렌더링
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// 서비스워커 등록
+registerSW({ immediate: true });
